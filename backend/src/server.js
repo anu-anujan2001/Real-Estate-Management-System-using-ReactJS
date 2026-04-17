04346ef529af302e43d685d53923764097348e8c
Anujan <anujan20010504@gmail.com>
2026-04-17 18:02:28 +0530

    inital backend created successfully

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const cookieParser = require('cookie-parser');
+
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
